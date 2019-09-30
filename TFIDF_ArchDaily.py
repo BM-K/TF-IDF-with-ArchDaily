a9cf57e087cf151c07b4492b4de072c6f4fd7abe
bongminkim <klbm126@gmail.com>
2019-09-30 19:01:35 +0900

    tfidf_archdaily_20190930

--- TFIDF_ArchDaily.py
+++ TFIDF_ArchDaily.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 def get_recommendations(title, cosine_sim):
-    # 선택한 영화의 타이틀로부터 해당되는 인덱스를 받아옵니다. 이제 선택한 영화를 가지고 연산할 수 있습니다.
+    
     while True:
         try:
             idx = indices[title]
@@ -15,24 +15,22 @@ def get_recommendations(title, cosine_sim):
     idx = indices[title]
     idx = int(idx)
 
-    # 모든 영화에 대해서 해당 영화와의 유사도를 구합니다.
+
     sim_scores = list(enumerate(cosine_sim[idx]))
 
-    # 유사도에 따라 영화들을 정렬합니다.
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
 
-    # 가장 유사한 20개의 영화를 받아옵니다.
+
     sim_scores = sim_scores[1:21]
 
-    # 가장 유사한 10개의 영화의 인덱스를 받아옵니다.
+
     movie_indices = [i[0] for i in sim_scores]
     num=1
     for i in movie_indices:
         print(num,":",data_Name[i])
         num+=1
     print("------------------------------------------------------")
-    # # 가장 유사한 10개의 영화의 제목을 리턴합니다.
-    # return data['title'].iloc[movie_indices]
+
 
 def get_page_myword(word, sentence):
     for i in range(len(data_Des)):
